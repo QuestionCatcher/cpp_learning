8b8de6d2911305bc38502bffd8d25a92a026fe1b
QuestionCatcher <maciekp044@gmail.com>
2024-03-27 17:44:32 +0100

    Added a condition that checks for the correct entry of the world 'welcome'

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@ int main() {
     string A;
     cout << "Type 'welcome'" << endl;
     cin >> A;
-    cout << "Welcome in C++ world! Enjoy your adventure" << endl;
+    if (A == "welcome"){
+
+    } else {
+        cout << "Misspelled word! Type 'welcome'" << endl;
+        cin >> A;
+        cout << "Welcome in C++ world! Enjoy your adventure" << endl;
+    }
 
 }
